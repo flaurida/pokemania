f0316a6680470b278561a8c86f1c49c78fe02897
Laura Cressman <lauracressman@mac.com>
2017-03-06 15:37:10 -0500

    fix babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,10 @@ module.exports = {
      {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2016']
+        }
       }
     ]
   },
