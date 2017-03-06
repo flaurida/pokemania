826b564489fe65bc6b23a73d6e50a8cf0acd0397
Laura Cressman <lauracressman@mac.com>
2017-03-05 21:23:09 -0500

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
   	filename: "./client/bundle.js"
   },
+  module: {
+   loaders: [
+     {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
   devtool: 'source-map'
 };
